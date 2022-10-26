559dbf9bef0d9d0603d0f9de14b4f29affed4736
mohdrizwanuddin <mohdrizwanuddin06@gmail.com>
2022-10-26 15:55:49 +0530

    first update

---
+++ src/main/java/com/caprusit/SpringBootGitHubExampleApplication.java
@@ -0,0 +1,13 @@
+package com.caprusit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitHubExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitHubExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caprusit/SpringBootGitHubExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caprusit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGitHubExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
